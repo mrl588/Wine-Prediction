1cc18508fafe4212a53c4cb76a863a38a5b323d1
bgao8 <bgao2005@gmail.com>
2026-04-29 15:00:14 -0400

    Removed redundancies, cleaned up code.

--- _wine_data.py
+++ _wine_data.py
@@ -6,7 +6,16 @@ wine_quality = fetch_ucirepo(id=186)
 # data (as pandas dataframes) 
 X = wine_quality.data.features 
 y = wine_quality.data.targets 
-  
+
+df = X.join(y)
+df.columns = df.columns.str.strip().str.replace(" ", "_")
+
+if "Id" in df.columns:
+    df = df.drop(columns=["Id"])
+
+X = df.drop(columns=["quality"])
+y = df[["quality"]]
+ 
 # metadata 
 print(wine_quality.metadata) 
   
